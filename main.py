76de52d0b385af91051c2951b47dde15a1252fd5
MOSAWIx <sawimo71@gmail.com>
2024-01-18 23:57:24 +0100

    saf dert def li atbyen lina data baqi anzedo lweqt safi

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class Main:
         
         
         
-def get_data(self):
+    def get_data(self):
         
         self.url_api = requests.get(self.url.format(self.city_name.get(),self.API))
         if self.url_api:
@@ -53,10 +53,29 @@ def get_data(self):
                 self.city , self.country , self.temp_klv , self.temp_celcuis , self.temp_fehr , self.weather ,self.pressure , self.description , self.humidity
             )       
         
-                
+            if self.final_result:
+                self.show_data()    
+        
+        
+    def show_data(self):
+        self.object_style.lbl1['text']=self.final_result[0]+'-'+self.final_result[1]
+        self.object_style.lbl2['text']=("{:.0f}°C , {:.0f}°F".format(self.final_result[3] , self.final_result[4]))
+        self.object_style.lbl3['text']= self.final_result[5]
+        self.object_style.lbl4['text']= self.final_result[6]
+        self.object_style.lbl5['text']=self.final_result[7]
+        self.object_style.lbl6['text']='| humidity'
+        
+        
+        self.label_temp_celcuis = Label(self.root, font=('popins',25),bg='white',fg='red',text="{:.0f}°C".format(self.final_result[3]))
+        self.label_temp_celcuis.place(x=500,y=200)
+        
+        
+        self.label_humidity = Label(self.root , text=f"{self.final_result[8]}",font=('poppins',16),fg='black').place(x=550,y=305)
         
         
         
+        
+            
 
 if __name__ == "__main__": 
   app = Tk()
