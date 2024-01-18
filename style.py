ce27a9d2a94685cee1da4f875eb0e118ce4cf350
JBARA999 <makhtarjbara1213@gmail.com>
2024-01-18 22:46:23 +0100

    rah  zadt dok tsawor li ba9in

--- style.py
+++ style.py
@@ -15,3 +15,10 @@ class Style:
         self.image_city = PhotoImage(file='images//search.png')
         self.label_city = Label(self.master , image=self.image_city )
         self.label_city.place(x=250,y=40)
+
+
+        self.logo_image = PhotoImage(file='images//logo.png')
+        self.label_logo = Label(self.master , image=self.logo_image ).place(x=300,y=200)
+
+        self.box_image = PhotoImage(file='images//box.png')
+        self.box_logo = Label(self.master , image=self.box_image ).place(x=120,y=640)
